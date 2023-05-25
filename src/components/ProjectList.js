bd6516c8fb8f7c0309cc01800d13562f30ea344e
Harpreet Singh <Harpreets1201@gmail.com>
2023-05-25 01:19:44 -0400

    Completed lab

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -1,19 +1,26 @@
 import React from "react";
+import ProjectItem from "./ProjectItem";
 
 
 function ProjectList({ projects }) {
   console.log(projects);
-  
+
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      {projects.map((proj) => {
-        console.log(proj);
+
+      {projects.map((projectInfo) => {
         return (
-          
+          <ProjectItem
+            key={projectInfo.id}
+            name={projectInfo.name}
+            about={projectInfo.about}
+            technologies={projectInfo.technologies}
+          />
         );
+      })}
     </div>
-  ))
+  );
 }
 
 export default ProjectList;
